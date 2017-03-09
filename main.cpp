57760174afb9d53045619c140e4c42d2aa48620b
Антон Епишкин <YepishkinAP@mpei.ru>
2017-03-09 15:05:47 +0300

    Мы изменили и добавили файл main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+using namespace std;
+
 int
 main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int a, b;
+    cout << "Enter A and B: ";
+    cin >> a >> b;
 }
