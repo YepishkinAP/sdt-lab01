05cb2566204f4a5826bbcb2a86e8f851f09c75b8
Антон Епишкин <YepishkinAP@mpei.ru>
2017-03-09 16:23:57 +0300

    Мы добавили в программу печать A*2.

--- main.cpp
+++ main.cpp
@@ -7,4 +7,5 @@ main() {
     int a, b;
     cout << "Enter A and B: ";
     cin >> a >> b;
-    cout << "A + B = " << a + b << '\n';}
+    cout << "A + B = " << a + b << '\n';
+    cout << "2*A = " << 2*a;}
